d0ce53e9563d44a4de68c80a699fb844cef61c8b
JBlackCat <blackjes@gmail.com>
2017-09-17 17:36:46 -0700

    static fixup

--- app/components/filterable-product-table/product-table/product-row.js
+++ app/components/filterable-product-table/product-table/product-row.js
@@ -2,13 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNameBindings:['outOfStock'],
-
-	init() {
-		this._super(...arguments);
-
-		const model = this.get('productAttr');
-
-		//Set outOfStock classname binding boolean
-		this.set('outOfStock', model.stocked);
-	}
+	outOfStock: Ember.computed('productAttr', function() {
+		return this.get('productAttr').stocked;
+	}),
 });
